dd143df292dd00110a869c6b9f6f0c665841e4d1
Donald Sebastian Leung <donaldsebleung@gmail.com>
2023-10-06 14:16:07 +0000

    Mention license in README

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm <strong>Donald</strong>. I'm a Cloud Technical Specialist specializing in AWS, Alibaba Cloud, GitLab and Kubernetes. You can find me on <a href="https://github.com/DonaldKellett">GitHub</a>.</p>
+        <p>Hello, I'm <strong>Donald</strong>. I'm a DevOps engineer specializing in AWS, Alibaba Cloud, GitLab and Kubernetes. You can find me on <a href="https://github.com/DonaldKellett">GitHub</a>.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
